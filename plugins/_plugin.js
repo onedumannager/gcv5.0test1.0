d423eaab2bcba08e78a34691e38ed251365970cc
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-27 13:56:17 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -28,63 +28,7 @@ const heroku = new Heroku({
 
 let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
 
-Asena.addCommand({pattern: 'install ?(.*)', fromMe: true, desc: Lang.INSTALL_DESC, warn: Lang.WARN}, (async (message, match) => {
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_URL + '.install https://gist.github.com/phaticusthiccy/4232b1c8c4734e1f06c3d991149c6fbd')
-    try {
-        var url = new URL(match[1]);
-    } catch {
-        return await message.sendMessage(Lang.INVALID_URL);
-    }
-    
-    if (url.host === 'gist.github.com') {
-        url.host = 'gist.githubusercontent.com';
-        url = url.toString() + '/raw'
-    } else {
-        url = url.toString()
-    }
 
-    var response = await got(url);
-    if (response.statusCode == 200) {
-        // plugin adı
-        var plugin_name = response.body.match(/addCommand\({.*pattern: ["'](.*)["'].*}/);
-        if (plugin_name.length >= 1) {
-            plugin_name = "__" + plugin_name[1];
-        } else {
-            plugin_name = "__" + Math.random().toString(36).substring(8);
-        }
-
-        fs.writeFileSync('./plugins/' + plugin_name + '.js', response.body);
-        try {
-            require('./' + plugin_name);
-        } catch (e) {
-            fs.unlinkSync('/root/WhatsAsenaDuplicated/plugins/' + plugin_name + '.js')
-            return await message.sendMessage(Lang.INVALID_PLUGIN + ' ```' + e + '```');
-        }
-
-        await Db.installPlugin(url, plugin_name);
-        await message.client.sendMessage(message.jid, Lang.INSTALLED, MessageType.text);
-        if (!match[1].includes('phaticusthiccy')) {
-            await new Promise(r => setTimeout(r, 400));
-            await message.client.sendMessage(message.jid, Lang.UNOFF, MessageType.text);
-        }
-    }
-}));
-
-Asena.addCommand({pattern: 'plugin', fromMe: true, desc: Lang.PLUGIN_DESC }, (async (message, match) => {
-    var mesaj = Lang.INSTALLED_FROM_REMOTE;
-    var plugins = await Db.PluginDB.findAll();
-    if (plugins.length < 1) {
-        return await message.sendMessage(Lang.NO_PLUGIN);
-    } else {
-        plugins.map(
-            (plugin) => {
-                let vf = plugin.dataValues.url.includes('phaticusthiccy') ? msg : inmsg
-                mesaj += '```' + plugin.dataValues.name + '```: ' + plugin.dataValues.url + '\n' + vf + '\n\n';
-            }
-        );
-        return await message.client.sendMessage(message.jid, mesaj, MessageType.text);
-    }
-}));
 
 Asena.addCommand({pattern: 'remove(?: |$)(.*)', fromMe: true, desc: Lang.REMOVE_DESC}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.NEED_PLUGIN);
