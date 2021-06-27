06100e0611d4093d92eaf484669a64385180dab9
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-27 13:59:27 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -17,7 +17,7 @@ const need = "*type some word after command*\n*විධානයට පසු�
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'waifu ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'anime ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
         var ttinullimage = await axios.get(`https://bx-${Config.ON}.herokuapp.com/api/sfw/waifu?apikey=${Config.CCN}`, { responseType: 'arraybuffer' })
 
@@ -28,7 +28,7 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'waifu ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'anime ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
 
         var ttinullimage = await axios.get(`https://bx-${Config.ON}.herokuapp.com/api/sfw/waifu?apikey=${Config.CCN}`, { responseType: 'arraybuffer' })
 
