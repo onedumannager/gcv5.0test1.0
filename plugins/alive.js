5714a2df35b39c97438ffc9da67b4f636a221619
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-27 16:06:58 +0530

    Update and rename system_stats.js to alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,10 +1,4 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-Developer & Co-Founder - Phaticusthiccy
+/*
 */
 
 const Asena = require('../events');
@@ -27,13 +21,6 @@ if (Config.WORKTYPE == 'private') {
         await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => { await message.client.sendMessage(message.jid, res.data, MessageType.image, { caption: "»»————　♔　———-««",Config.ALIVEMSG }); });
     }));
 
-    Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
-
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
 }
 else if (Config.WORKTYPE == 'public') {
 
@@ -43,13 +30,5 @@ else if (Config.WORKTYPE == 'public') {
         try { pp = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); } catch { pp = await message.client.getProfilePicture(); }
         await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => { await message.client.sendMessage(message.jid, res.data, MessageType.image, { caption: "»»————　♔　———-««",Config.ALIVEMSG }); });
     }));
-
-    Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
-
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
-    
+ 
 }
