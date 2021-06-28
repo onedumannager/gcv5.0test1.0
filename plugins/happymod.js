8daad068e68e3be0768a75df490b56a1ac95eea9
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-28 17:44:00 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -12,23 +12,32 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 if (Config.WORKTYPE == 'private') {
-Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, }, async (message, match) => {
+Asena.addCommand({pattern: 'hpmod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, }, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
 	const url = `https://api.zeks.xyz/api/happymod?apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs&q=${match[1]}`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
-		'*📕 ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n\n' + 
-		'*📘 ' + Lang.Rate +'* ```' + json.result[0].rating + '```\n\n\n' + 
-		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[0].url + '```\n', MessageType.text);
+		if (response.statusCode === 200) return 
+		
+		await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data),  MessageType.image,{mimetype: Mimetype.png, caption:
+		'*📕 ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n' + 
+		'*📘 ' + Lang.Rate +'* ```' + json.result[0].rating + '```\n' + 
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[0].url + '```\n\n'+
+		'*📕 ' + Lang.NAMEY +'* ```' + json.result[1].title + '```\n' + 
+		'*📘 ' + Lang.Rate +'* ```' + json.result[1].rating + '```\n' + 
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[1].url + '```\n\n'+
+                '*📕 ' + Lang.NAMEY +'* ```' + json.result[2].title + '```\n' + 
+		'*📘 ' + Lang.Rate +'* ```' + json.result[2].rating + '```\n' + 
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[2].url + '```\n'});
+		
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
 });
 }
 else if (Config.WORKTYPE == 'public') {
-	Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC,  }, async (message, match) => {
+	Asena.addCommand({pattern: 'hpmod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC,  }, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
 	const url = `https://api.zeks.xyz/api/happymod?apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs&q=${match[1]}`;
 	try {
