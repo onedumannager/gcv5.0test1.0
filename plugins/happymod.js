105da850dbcaf177f607198ba55d88bf1aedb7b0
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-28 14:46:45 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -14,7 +14,7 @@ const Lang = Language.getString('weather');
 if (Config.WORKTYPE == 'private') {
 Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, }, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
+	const url = `https://api.zeks.xyz/api/happymod?apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs&q=${match[1]}`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
@@ -30,7 +30,7 @@ Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC
 else if (Config.WORKTYPE == 'public') {
 	Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC,  }, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
+	const url = `https://api.zeks.xyz/api/happymod?apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs&q=${match[1]}`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
