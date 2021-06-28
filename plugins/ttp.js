25c57abda2862af6d1477586402f4eb9aba426c8
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-29 01:39:24 +0530

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,7 +1,7 @@
 /* 
 */
 
-const thiccysapi = require('textmaker-thiccy'); // Import NPM Package
+const thiccysapi = require('textmaker-lasi'); // Import NPM Package
 
 const Asena = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
