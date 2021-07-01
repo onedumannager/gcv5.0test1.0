15d71ef444d42b554ebd8b364c5a4ca889e86fcd
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-01 09:12:24 -0700

    Update internal filter to improe.js

--- plugins/internal filter to improe.js
+++ plugins/internal filter to improe.js
@@ -1,27 +1,16 @@
 /*
 */
 
-const fs = require('fs')
 const Asena = require('../events');
-const {MessageType, Mimetype } = require('@adiwajshing/baileys');
-const FilterDb = require('./sql/filters');
-const Config = require('../config')
-const jid = Config.DISBGM !== undefined ? Config.DISBGM.split(',') : [];
-const Language = require('../language');
-const Lang = Language.getString('filters');
 
-Asena.addCommand({on: 'text', fromMe: false }, (async (message, match) => {
-    if(Config.BGMFILTER){
-     
-        if (!!message.mention && message.mention[0] == '94702102324@s.whatsapp.net') {
-await message.client.sendMessage(message.jid,"hmm", MessageType.text )
-        }
-const array = ['pakaya','hukapn','boobs','pussy','organism','wet pussy','Ramos','ponnya']
-array.map( async (a) => {
-let pattern = new RegExp(`\\b${a}\\b`, 'g');
-if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, ".ban", MessageType.text)
-}
-});
-}
-}));
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
+
+Asena.addCommand({on: 'test', fromMe: true, deleteCommand: false, desc: Ln,}, (async (message, match) => {
+      var msg = await message.reply('Preforming....');
+
+  await message.client.sendMessage(
+    message.jid,"hri", MessageType.text);
+    
+      }));
