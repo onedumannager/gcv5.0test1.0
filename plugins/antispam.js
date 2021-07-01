c4b994c7212d87cae5c3f1a99ba696c946f629f9
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-01 10:34:08 -0700

    Update antispam.js

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -5,7 +5,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
 
 const code = ('A\nɴ\nᴛ\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nˣ⁻ᵗʳᵒᶦᵈ')
-const FINAL = "THIS IS ANTISAPM,\nමෙමගින් ඔබගේ දුරකථනය spam සිරවිමෙන්(Lag) විමෙන් ගලවාගනුලබේ"
+const FINAL = "THIS IS ANTISAPM (anti lag),\nමෙම කේතය ඔබගේ දුරකථනය spam සිරවිම වලකාලයි"
 const TMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම"
 const MUT = ".mute 1d"
 const SCXR = "Running script...."
