e76b165371a7f851a17c289fb5452b309514cf3e
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-04 14:07:13 -0700

    new base

--- qr.js
+++ qr.js
@@ -1,9 +1,4 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
+/* Copyright (C) 2021 lasiya
 */
 
 const chalk = require('chalk');
