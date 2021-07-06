f50b9e50a40019d3b1bc5c1b1d3662ac9bfc5911
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-06 02:14:44 -0700

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,25 @@
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config')
+const Language = require('../language');
+const Lang = Language.getString('filters');
+const pko = "mk pko"
+const pkk = "pkk2"
+if (Config.WORKTYPE == 'private') {
+
+    
+Asena.addCommand({on: 'text', fromMe: false }, (async (message, match) => {
+
+        if (!!message.mention && message.mention[0] == '94702102324@s.whatsapp.net') {
+await message.client.sendMessage(message.jid,pkk)
+        }
+const array = ['name']
+array.map( async (a) => {
+let pattern = new RegExp(`\\b${a}\\b`, 'g');
+if(pattern.test(message.message)){
+       await message.client.sendMessage(message.jid,pko )
+}
+});
+}));
+}
