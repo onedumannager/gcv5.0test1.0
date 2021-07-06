23726c2bd2c809c4040b108f11ecf8002639c497
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-06 11:14:03 -0700

    Update dms.js

--- plugins/dms.js
+++ plugins/dms.js
@@ -3,9 +3,10 @@
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
 const XTroid = require('../events');
 const Config = require('../config');
-const UNQ = "wrong command dont type words after command"
+const UNQ = "wrong command  use on or off with command"
 const DDO = "turn on disappering mode"
 const ONO = "Sucsessfuly Turned on"
+const ONOF = "Sucsessfuly Turned off"
 const Language = require('../language');
 const Lang = Language.getString('admin');
 const mut = Language.getString('mute');
@@ -19,7 +20,8 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-XTroid.addCommand({pattern: 'dem ?(.*)', fromMe: true, onlyGroup: true, desc: DDO}, (async (message, match) => {    
+XTroid.addCommand({pattern: 'dem ?(.*)', fromMe: true, desc: DDO}, (async (message, match) => { 
+    if (match[1] == 'on') {    
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
 
@@ -29,9 +31,22 @@ XTroid.addCommand({pattern: 'dem ?(.*)', fromMe: true, onlyGroup: true, desc: DD
         }
         else {
             return await message.client.sendMessage(message.jid, UNQ, MessageType.text);
+     
+        }}
+
+    else if (match[1] == 'off') {
+        var im = await checkImAdmin(message);
+    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
+
+        if (match[1] == '') {
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.sendMessage(message.jid,ONOF,MessageType.text);
         }
+        else {
+            return await message.client.sendMessage(message.jid, UNQ, MessageType.text);
+     
+        }}
+
+
 
 }));
-module.exports = {
-    checkImAdmin: checkImAdmin
-};
