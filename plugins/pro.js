9041190235e81e8c62859b8c3b4b02168225bb8f
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-10 03:17:43 -0700

    Update pro.js

--- plugins/pro.js
+++ plugins/pro.js
@@ -10,7 +10,7 @@ const request = require('request');
 
 
 if (Config.WORKTYPE == 'private') {
-XTroid.addCommand({pattern: '1test ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+XTroid.addCommand({pattern: 'nlite ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
     lasiapi.textpro("https://textpro.me/create-3d-neon-light-text-effect-online-1028.html",
         `${match[1]}`
         ).then(async (data) => { 
@@ -29,4 +29,52 @@ XTroid.addCommand({pattern: '1test ?(.*)', fromMe: true, dontAddCommandList: tru
           } 
     });
 }));
+
+
+
+XTroid.addCommand({pattern: 'glite ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    lasiapi.textpro("https://textpro.me/green-neon-text-effect-874.html",
+        `${match[1]}`
+        ).then(async (data) => { 
+          try { 
+              var download = async(uri, filename, callback) => {
+                  await request.head(uri, async(err, res, body) => {    
+                      await request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                  });
+              };
+
+              await download(`${data}`, '/root/lizy/glite.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/glite.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              })
+          } catch(err) { 
+              console.log(err)
+          } 
+    });
+}));
+
+
+
+XTroid.addCommand({pattern: 'plux ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    lasiapi.textpro("https://textpro.me/color-full-luxury-metal-text-effect-969.html",
+        `${match[1]}`
+        ).then(async (data) => { 
+          try { 
+              var download = async(uri, filename, callback) => {
+                  await request.head(uri, async(err, res, body) => {    
+                      await request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                  });
+              };
+
+              await download(`${data}`, '/root/lizy/plux.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/plux.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
+              })
+          } catch(err) { 
+              console.log(err)
+          } 
+    });
+}));
+
+
+
+
 }
