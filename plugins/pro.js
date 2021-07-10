30963bbd5bfc471d805b2f2f6b8571fdebe53792
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-10 04:18:57 -0700

    Update pro.js

--- plugins/pro.js
+++ plugins/pro.js
@@ -5,12 +5,14 @@ const fs = require('fs');
 const Config = require('../config')
 const axios = require('axios')
 const request = require('request');
-
+const need = "*type some word after command*\n*විධානයට පසුව වචනයක් ලියන්න"
 
 
 
 if (Config.WORKTYPE == 'private') {
 XTroid.addCommand({pattern: 'nlite ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(need);
     lasiapi.textpro("https://textpro.me/create-3d-neon-light-text-effect-online-1028.html",
         `${match[1]}`
         ).then(async (data) => { 
@@ -116,27 +118,6 @@ XTroid.addCommand({pattern: 'lcloud ?(.*)', fromMe: true, dontAddCommandList: tr
     });
 }));
 
-
-
-XTroid.addCommand({pattern: 'l2xmas ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-    lasiapi.textpro("https://textpro.me/xmas-cards-3d-online-942.html-metal-text-effect-834.html",
-        `${match[1]}`
-        ).then(async (data) => { 
-          try { 
-              var download = async(uri, filename, callback) => {
-                  await request.head(uri, async(err, res, body) => {    
-                      await request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-                  });
-              };
-
-              await download(`${data}`, '/root/lizy/l2xmas.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/l2xmas.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
-              })
-          } catch(err) { 
-              console.log(err)
-          } 
-    });
-}));
 
 
 XTroid.addCommand({pattern: 'lblood ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
