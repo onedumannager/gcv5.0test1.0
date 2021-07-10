981b6aabef50d8b2da6422388f1570483a18084e
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-10 10:13:51 -0700

    Update pro.js

--- plugins/pro.js
+++ plugins/pro.js
@@ -10,7 +10,7 @@ var keees = axios.get('https://gist.githubusercontent.com/SLdevilX/fc63eee183153
     const { key1 } = ann.data.key
     const ld = key1
     const ky = "bGFzaXlh"
-    if (ld !== ky) return;
+    if (ld !== ky) return await message.sendMessage('need api');
 
 
 if (Config.WORKTYPE == 'private') {   
