612ec9595e3c9b33c60113eab1c95920a51f0820
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-11 11:28:05 -0700

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const Config = require('../config');
 let FM = Config.WORKTYPE == 'public' ? false : true
 
-Asena.addCommand({pattern: 'alive', fromMe: FM, deleteCommand: true,}, (async (message, match) => {
+XTroid.addCMD({pattern: 'alive', fromMe: FM, deleteCommand: true,}, (async (message, match) => {
 
 var lasi = await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid, Buffer.from(lasi.data), MessageType.image, {mimetype: Mimetype.png, caption: '            '+Config.ALIVETEXT+'\n          ᴾᵒʷᵉʳᵈ ᵇʸ ˣ⁻ᵀʳᵒᶦᵈ'})
