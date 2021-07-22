0e5c51ebd6ba6a9a4fe60587ae524387c32e622a
ngmg2022 <78734430+ngmg2022@users.noreply.github.com>
2021-07-22 07:17:54 +0530

    Update dms.js

--- plugins/dms.js
+++ plugins/dms.js
@@ -4,11 +4,12 @@ const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
 const XTroid = require('../events');
 const Config = require('../config');
 const UNQ = "wrong command dont type words after command"
-const DDO = "turn on disappering mode"
-const ONO = "Sucsessfuly Turned on"
+const DDO = "Bombing..."
+const ONO = ",tg We are Deamons😈 Do not run away... And remember some one"
 const Language = require('../language');
 const Lang = Language.getString('admin');
 const mut = Language.getString('mute');
+
 
 async function checkImAdmin(message, user = message.client.user.jid) {
     var grup = await message.client.groupMetadata(message.jid);
@@ -19,34 +20,45 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-XTroid.addCMD({pattern: 'dem ?(.*)', fromMe: true, desc: DDO}, (async (message, match) => {    
-    var im = await checkImAdmin(message);
-    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
-
+XTroid.addCMD({pattern: 'dem ?(.*)', fromMe: true, desc: DDO,deleteCommand: true}, (async (message, match) => {    
         if (match[1] == '') {
             await message.client.toggleDisappearingMessages(message.jid, 604800);
-            await message.client.sendMessage(message.jid,ONO,MessageType.text);
-        }
-        else {
-            return await message.client.sendMessage(message.jid, UNQ, MessageType.text);
-        }
-
-}));
-
-
-XTroid.addCMD({pattern: 'fdem ?(.*)', fromMe: true, desc: DDO}, (async (message, match) => {    
-    var im = await checkImAdmin(message);
-    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
-
-        if (match[1] == '') {
             await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.sendMessage(message.jid,ONO,MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
         }
         else {
             return await message.client.sendMessage(message.jid, UNQ, MessageType.text);
         }
 
 }));
+
+
+XTroid.addCMD({pattern: 'fdem ?(.*)', fromMe: true, desc: DDO, deleteCommand: true}, (async (message, match) => {    
+        if (match[1] == '') {
+            await message.client.sendMessage(message.jid,".tg",MessageType.text);
+            await message.client.sendMessage(message.jid,ONO,MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            
+        }
+        else {
+            return await message.client.sendMessage(message.jid, UNQ, MessageType.text);
+        }
+
+}));
+
+
+
+
+
+          
+
+
+
 module.exports = {
     checkImAdmin: checkImAdmin
 };
